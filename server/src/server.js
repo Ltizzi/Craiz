0a9903553cab35bc0d3235e4b36919b4ac638aad
ltizzi <terlizzileonardo@gmail.com>
2023-02-08 19:57:38 -0300

    added dependencies

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,10 @@
 const http = require("http");
+require("dotenv").config();
 const app = require("./app");
+
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 4246;
+const PORT = process.env.PORT || 2156;
 
 async function startServer() {
   server.listen(PORT, () => {
