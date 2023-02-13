781144944dbe521ce03e756d13b9a85fd8c39bf3
ltizzi <terlizzileonardo@gmail.com>
2023-02-13 11:40:40 -0300

    implemented first version of user model methods

--- server/src/models/users/users.model.js
+++ server/src/models/users/users.model.js
@@ -1,16 +1,63 @@
 const userRepo = require("./users.mongo");
+// const axios = require("axios");
 
-const axios = require("axios");
+const DEFAULT_USER_ID = 1;
 
-async function getAllUsers(skip, limit) {}
+async function getAllUsers(skip, limit) {
+  return await userRepo
+    .find({}, { _id: 0, __v: 0 })
+    .sort({ userId: 1 })
+    .skip(skip)
+    .limit(limit);
+}
 
-async function getUserById(id) {}
+async function getUserById(id) {
+  return await findUser({
+    userId: id,
+  });
+}
 
-async function getLastUserId() {}
+async function getLastUserId() {
+  const lastUser = await userRepo.findOne().sort("-userId");
+  if (!lastUser) {
+    return DEFAULT_USER_ID;
+  }
+  return lastUser.userId;
+}
 
-async function saveUser(user) {}
+async function saveUser(user) {
+  const newUserId = (await getLastUserId()) + 1;
+  const newUser = Object.assign(user, {
+    userId: newUserId,
+    isAdmin: false,
+    createdAt: new Date.now(),
+    softDeleted: false,
+  });
+  await userRepo.findOneAndUpdate(
+    {
+      userId: newUser.userId,
+    },
+    newUser,
+    { upsert: true }
+  );
+}
 
-async function deleteUser(id) {}
+async function deleteUser(id) {
+  const softDeleted = await userRepo.updateOne(
+    {
+      userId: id,
+    },
+    {
+      softDeleted: true,
+      updatedAt: new Date.now(),
+    }
+  );
+  return softDeleted.acknowledged === true && softDeleted.modifiedCount === 1;
+}
+
+async function findUser(filter) {
+  return await userRepo.findOne(filter);
+}
 
 module.exports = {
   getAllUsers,
