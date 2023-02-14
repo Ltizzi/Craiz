77b115cc246efb329aad10c9da74cb39a2e90759
Leonardo <terlizzileonardo@gmail.com>
2023-02-14 18:47:26 -0300

    added the first version of the meme card template

--- web-client/src/App.vue
+++ web-client/src/App.vue
@@ -1,10 +1,14 @@
 <template>
-  <LikeButton></LikeButton>
-  <font-awesome-icon icon="fa-light fa-heart" />
+  <div class="flex flex-col items-center justify-center">
+    <MemeCard></MemeCard>
+    <MemeCard></MemeCard>
+    <MemeCard></MemeCard>
+  </div>
 </template>
 
 <script setup lang="ts">
   import LikeButton from "./components/common/LikeButton.vue";
+  import MemeCard from "./components/layout/MemeCard.vue";
 </script>
 
 <style scoped></style>
