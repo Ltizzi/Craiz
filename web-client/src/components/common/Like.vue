fdbc44201a869edad9da282b2f22ab1bffc23266
Leonardo <terlizzileonardo@gmail.com>
2023-02-14 14:25:12 -0300

    added base button component, fontawesome icons and tailwindcss configuration to vue project

---
+++ web-client/src/components/common/Like.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <BaseButton></BaseButton>
+  </div>
+</template>
+<script setup lang="ts">
+  import { defineComponent } from "vue";
+  import BaseButton from "./BaseButton.vue";
+</script>
+<style lang=""></style>
