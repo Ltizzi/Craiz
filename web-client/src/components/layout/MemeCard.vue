915b5b0f8bd4b219e661972f21377dac28268449
Leonardo <terlizzileonardo@gmail.com>
2023-02-17 17:49:28 -0300

    the generate meme button makes a json object to send to the api. Added container class for responsive design in memecard element and create meme modal

--- web-client/src/components/layout/MemeCard.vue
+++ web-client/src/components/layout/MemeCard.vue
@@ -1,6 +1,6 @@
 <template lang="">
-  <div class="my-3 flex w-1/3 flex-col border-2 p-5 shadow-md">
-    <div class="flex flex-row">
+  <div class="container my-3 flex w-1/3 flex-col border-2 p-5 shadow-md">
+    <div class="container flex flex-row">
       <img :src="`${userInfo.avatar}`" alt="" class="mr-2 w-12" />
       <h3 class="ml-1 pt-1 text-2xl font-bold">{{ userInfo.name }}</h3>
       <h4 class="pt-2 pl-2 text-lg italic">{{ userInfo.username }}</h4>
