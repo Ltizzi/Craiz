978e134aafae69ee314c2c12960b3b0e708f65bd
Leonardo <terlizzileonardo@gmail.com>
2023-03-01 18:40:44 -0300

    added a commentsList component to render all comments for a meme and a MemeView component to include the memeCard and its comments

---
+++ web-client/src/components/layout/CommentsView.vue
@@ -0,0 +1,8 @@
+<template lang="">
+  <MemeCard></MemeCard>
+</template>
+
+<script setup lang="ts">
+  import MemeCard from "./MemeCard.vue";
+</script>
+<style lang=""></style>
---
+++ web-client/src/views/TheMeme.vue
@@ -0,0 +1,9 @@
+<template lang="">
+  <div class="flex flex-col justify-center">
+    <MemeCard></MemeCard>
+  </div>
+</template>
+<script setup lang="ts">
+  import MemeCard from "@/components/layout/MemeCard.vue";
+</script>
+<style lang=""></style>
