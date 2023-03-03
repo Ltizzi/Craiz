6e751f92b4c69f63f697b926d6bef94ed89c004b
Leonardo <terlizzileonardo@gmail.com>
2023-03-03 20:28:29 -0300

    started with implementations of CommentsView component

--- web-client/src/views/TheMeme.vue
+++ web-client/src/views/TheMeme.vue
@@ -1,9 +1,19 @@
 <template lang="">
   <div class="flex flex-col justify-center">
-    <MemeCard></MemeCard>
+    <MemeCard :data="meme"></MemeCard>
+    <CommentsView></CommentsView>
   </div>
 </template>
 <script setup lang="ts">
   import MemeCard from "@/components/layout/MemeCard.vue";
+  import CommentsView from "@/components/layout/CommentsView.vue";
+  import { onMounted, ref } from "vue";
+  import { useMemesStore } from "@/store/memes";
+
+  const memesStore = useMemesStore();
+  let meme = ref(memesStore.meme);
+
+  console.log(meme.value);
+  // meme.value = memesStore.meme;
 </script>
 <style lang=""></style>
