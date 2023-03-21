c9eb7533a0c96a08ac03cf1e0d6c727acf36a15e
Leonardo <terlizzileonardo@gmail.com>
2023-03-21 13:44:42 -0300

    added the correct import for formData package

--- server/src/routes/utils/utils.controller.js
+++ server/src/routes/utils/utils.controller.js
@@ -2,6 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const path = require("path");
 const fs = require("fs");
+const FormData = require("form-data");
 //const { Blob } = require("buffer");
 require("dotenv").config();
 
