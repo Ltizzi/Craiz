e608cea5541c1731187363fff74e68d1558e46d7
Leonardo <terlizzileonardo@gmail.com>
2023-03-23 19:37:53 -0300

    update landing page to correct text overlaping the logo. Better style on lateral menu.

--- web-client/src/components/ui/CreateMemeButton.vue
+++ web-client/src/components/ui/CreateMemeButton.vue
@@ -1,6 +1,6 @@
 <template lang="">
   <BaseButton
-    class="mt-2 w-14 rounded-xl bg-emerald-500 py-1 text-sm font-bold text-white sm:w-3/12 sm:text-base lg:w-40"
+    class="mt-2 w-14 rounded-xl bg-emerald-500 py-2 text-sm font-bold text-white sm:w-3/12 sm:text-base lg:w-full lg:text-lg"
     @click="modalSwitch"
     >Crear Meme</BaseButton
   >
