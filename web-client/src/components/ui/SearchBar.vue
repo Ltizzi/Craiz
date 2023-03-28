262386e4a0839ed4a3d90b69a364e3c6933b4c18
Leonardo <terlizzileonardo@gmail.com>
2023-03-28 10:16:53 -0300

    added magnyficient glass to search bar component.

--- web-client/src/components/ui/SearchBar.vue
+++ web-client/src/components/ui/SearchBar.vue
@@ -1,18 +1,35 @@
 <template lang="">
-  <div class="flex w-full flex-row gap-2 bg-slate-700 py-3">
+  <div class="flex w-full flex-row gap-2 bg-slate-700 py-3" v-if="props.back">
     <div class="w-1/12">
-      <router-link to="/" v-if="props.back">
+      <router-link to="/">
         <BaseButton class="mx-5 my-auto text-lg text-white opacity-95">
           <font-awesome-icon icon="fa-solid fa-circle-chevron-left" />
         </BaseButton>
       </router-link>
     </div>
 
-    <div class="flex w-10/12 justify-center">
+    <div class="flex w-10/12 items-center justify-center">
       <input
         type="text"
         placeholder="Buscar en Craze"
-        class="w-11/12 rounded-2xl bg-slate-600 py-1 px-2 pl-5 text-white"
+        class="relative w-11/12 rounded-2xl bg-slate-600 py-1 px-2 pl-5 text-white"
+      />
+      <font-awesome-icon
+        icon="fa-solid fa-magnifying-glass"
+        class="absolute right-16 rounded-r-full bg-slate-500 py-2 pr-3 pl-2 text-gray-300"
+      />
+    </div>
+  </div>
+  <div class="flex w-full flex-row gap-2 bg-slate-700 py-3" v-else>
+    <div class="flex items-center justify-center">
+      <input
+        type="text"
+        placeholder="Buscar en Craze"
+        class="relative rounded-2xl bg-slate-600 py-1 px-2 pl-5 text-white"
+      />
+      <font-awesome-icon
+        icon="fa-solid fa-magnifying-glass"
+        class="absolute right-2 rounded-r-full bg-slate-500 py-2 pr-3 pl-2 text-gray-300"
       />
     </div>
   </div>
