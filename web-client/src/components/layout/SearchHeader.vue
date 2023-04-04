048f1c7099b756c6b9c5a40ca81f352aefad6f14
Leonardo <terlizzileonardo@gmail.com>
2023-04-04 15:38:28 -0300

    updated large screens responsive designs. Added dropzone file to upload meme. Change edit profile modal form to get a layour similar to the profile componenent.

--- web-client/src/components/layout/SearchHeader.vue
+++ web-client/src/components/layout/SearchHeader.vue
@@ -1,9 +1,11 @@
 <template lang="">
-  <div
-    class="fixed right-0 top-0 z-50 opacity-95 md:left-72 md:ml-1 md:w-6/12 lg:right-1/3 lg:-mr-5"
-  >
-    <SearchBar :back="true" />
-    <SearchNav />
+  <div class="lg:w-full">
+    <div
+      class="fixed right-0 top-0 z-50 w-full opacity-95 md:left-72 md:ml-1 md:w-6/12 lg:left-1/3 lg:right-2/3 lg:-ml-24 lg:w-2/5"
+    >
+      <SearchBar :back="true" />
+      <SearchNav />
+    </div>
   </div>
 </template>
 <script setup lang="ts">
